29d78191f3760d60cb6bda69f58e8e1eb687268b
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-11-19 09:29:59 +0300

    Update db.php

--- html/db.php
+++ html/db.php
@@ -4,5 +4,8 @@ $username = "root";
 $password = "root";
 $db = "Conectum";
 
-$conn = mysqli_connect($server, $username, $password, $db)
+$conn = mysqli_connect($server, $username, $password, $db);
+
+if(!$conn) {
+  fail!("Conection Failed!". mysql_connect_error());
 ?>
