e6f133a2557738f5e4a8ae3d7dc57f02e70f2a90
matveymayner <144268527+matvey-mayner@users.noreply.github.com>
2024-11-19 21:07:23 +0300

    Update db.php

--- html/db.php
+++ html/db.php
@@ -1,13 +1,13 @@
 <?php
 
-$servername="localhost";
-$username="root";
-$password="";
-$dbname="conectum";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "conectum";
 
-$conn=mysqli_connect($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if(!$conn){
-	 die("Connection Failed!". mysqli_connect_error());
+if (!$conn) {
+    die("Connection Failed: " . mysqli_connect_error());
 } 
 ?>
